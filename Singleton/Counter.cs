66b4d52ab4d0ca54dcb590fc5fed5761d8998fa7
Diego Magionami <d.magionami@sintraconsulting.eu>
2021-05-06 08:11:17 +0200

    added Singleton reference

---
+++ Singleton/Counter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public sealed class Counter
+    {
+        private static readonly Counter _instance = new Counter();
+
+        public Random random = new Random();
+        private int mainCounter = 0;
+        private int sumCounter = 0;
+        
+        private Counter()
+        {
+            mainCounter = random.Next(10);
+        }
+
+        public static Counter GetCounter()
+        {
+            return _instance;
+        }
+
+        public int GetNextNumber()
+        {
+            sumCounter += 1;
+            return mainCounter += 1;
+        }
+
+        public int GetCurrentNumber()
+        {
+            return mainCounter;
+        }
+
+        public int GetSumNumber()
+        {
+            return sumCounter;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Counter counter1 = Counter.GetCounter();
+            Counter counter2 = Counter.GetCounter();
+
+            Console.WriteLine("ENTER THE LOOP");
+            Console.WriteLine("The current number for third instance is {0}", GetCurrentCounter());
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.WriteLine("{0} LOOP", i);
+                Console.WriteLine("The next number for first instance is {0}", counter1.GetNextNumber());
+                Console.WriteLine("The next number for second instance is {0}", counter2.GetNextNumber());
+            }
+
+            Console.WriteLine("EXIT THE LOOP");
+            Console.WriteLine("The current number for third instance is {0}", GetCurrentCounter());
+            Console.WriteLine("The sum is {0}", GetSumCounter());
+            Console.WriteLine("The starting counter was {0}", (GetCurrentCounter() - GetSumCounter()));
+        }
+
+        private static int GetCurrentCounter()
+        {
+            Counter counter3 = Counter.GetCounter();
+            return counter3.GetCurrentNumber();
+        }
+
+        private static int GetSumCounter()
+        {
+            Counter counter4 = Counter.GetCounter();
+            return counter4.GetSumNumber();
+        }
+    }
+}
